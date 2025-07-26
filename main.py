0ea6ad34715f2f548409659c0fad24df25bcc1f1
Rogerio Campos <rogerio.campos07@gmail.com>
2025-07-26 19:50:10 -0300

    refatoração do objeto TickerScraper no método search_ticker e implementação do método search_ticker_output_csv

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from src.pesquisa_acoes.services.scrap import TickerScraper
 
 scraper = TickerScraper()
 
-scraper.search_ticker('BBAS3.SA')
+#scraper.search_ticker('BBAS3.SA')
+scraper.search_ticker_output_csv('BBAS3.SA')
--- src/pesquisa_acoes/services/scrap.py
+++ src/pesquisa_acoes/services/scrap.py
@@ -5,7 +5,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from time import sleep
+from csv import writer
+from datetime import datetime
+
 
 
 
@@ -43,6 +45,7 @@ class TickerScraper:
     def search_ticker(self, ticker):
         
         try:
+            informations = []
             search_box = self.driver.find_element(By.ID, "ybar-sbq")
             search_box.click()
             search_box.send_keys(ticker)
@@ -50,15 +53,34 @@ class TickerScraper:
             print('código da ação digitado')
             get_info_ticker = self.driver.find_element(By.XPATH,"//h1[@class='yf-4vbjci']")
             get_price_ticker = self.driver.find_element(By.XPATH,'//*[@id="nimbus-app"]/section/section/section/article/section[2]/div/ul/li[3]/span[2]')
+            stock_name = get_info_ticker.text
+            price = get_price_ticker.text.split()[0]
+            informations = [stock_name,price]
             print(get_info_ticker.text)
             print(get_price_ticker.text.split()[0])
             print('Informações obtidas')
+            return informations
 
         finally:
             print('Fechando navegador')
             self.driver.quit()
 
             
+    def search_ticker_output_csv(self, ticker):
+        dados = self.search_ticker(ticker)
+        stock_name = dados[0]
+        price = dados[1]
+        
+        
+        date = datetime.now().strftime('%d-%m-%Y')
+        time = datetime.now().strftime('%H:%M:%S')
+        informations = [stock_name,price,date,time]
+        try:
+            with open('tickers.csv','a',encoding='utf8',newline='') as f:
+                csv_writer = writer(f)
+                csv_writer.writerow(informations)
+        except:
+            pass
 
 
     
