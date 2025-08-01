de14da325277d9d6e17cd9b5e76d2779c5a0dd83
Rogerio Campos <rogerio.campos07@gmail.com>
2025-08-01 19:13:58 -0300

    Refatoração da classe TickerScraper para separar o criador de csv e deixa-la somente como Scraper

--- src/pesquisa_acoes/services/scrap.py
+++ src/pesquisa_acoes/services/scrap.py
@@ -2,11 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from csv import writer
 from datetime import datetime
+
 
 
 
@@ -45,40 +44,42 @@ class TickerScraper:
     def search_ticker(self, ticker):
         
         try:
-            informations = []
             search_box = self.driver.find_element(By.ID, "ybar-sbq")
             search_box.click()
             search_box.send_keys(ticker)
             search_box.send_keys(Keys.ENTER)
             print('código da ação digitado')
-            get_info_ticker = self.driver.find_element(By.XPATH,"//h1[@class='yf-4vbjci']")
-            get_price_ticker = self.driver.find_element(By.XPATH,"//li[span[@title='Bid']]/span[@class='value yf-1b7pzha']")
-            stock_name = get_info_ticker.text.split('(')[0]
-            cod_stock_name = get_info_ticker.text.split('(')[1][:-1].split('.')[0]
-            price = get_price_ticker.text.split()[0]
-            date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-            informations = [cod_stock_name,stock_name,price,date]
-            print(cod_stock_name)
-            print(stock_name)
-            print(price)
-            print('Informações obtidas')
-            print(informations)
-            return informations
+            
+            print('Obtendo informação')
+            try:
+                get_info_ticker = self.driver.find_element(By.XPATH,"//h1[@class='yf-4vbjci']")
+                get_price_ticker = self.driver.find_element(By.XPATH,"//li[span[@title='Bid']]/span[@class='value yf-1b7pzha']")
+                stock_name = get_info_ticker.text.split('(')[0]
+                cod_stock_name = get_info_ticker.text.split('(')[1][:-1].split('.')[0]
+                price = get_price_ticker.text.split()[0]
+                date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+                data = {}
+                data = {'cod_stock_name':cod_stock_name,'stock_name':stock_name,'price':price,'date':date}
+                print('Informações obtidas com sucesso')
+                
+
+            except NoSuchElementException:
+                print('Informações não encontradas')
+            
+            
+        except UnboundLocalError:
+            print('Não foi realizar a pesquisa')
 
         finally:
             print('Fechando navegador')
             self.driver.quit()
+            print('Até breve')
+        return data
+           
 
             
-    def search_ticker_output_csv(self, ticker):
-        dados = self.search_ticker(ticker)
-        
-        try:
-            with open('tickers.csv','a',encoding='utf8',newline='') as f:
-                csv_writer = writer(f)
-                csv_writer.writerow(dados)
-        except:
-            pass
+
+            
 
 
     
