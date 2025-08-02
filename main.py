772ac0cf19c1cc6080de1b941c915d457b8dcfb9
Rogerio Campos <rogerio.campos07@gmail.com>
2025-08-02 17:14:27 -0300

    fix: correção de bug

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from pprint import pprint as print
+from src.pesquisa_acoes.utils.escreve_csv import search_ticker_output_csv, search_tickers_output_csv
 from src.pesquisa_acoes.services.scrap import TickerScraper
 from src.pesquisa_acoes.services.clients import SearchTicker
 
-scraper = TickerScraper()
+#scraper = TickerScraper()
 client = SearchTicker()
 
 # Exemplo de scrap e registro 1 ticket em arquivo .csv
@@ -14,5 +14,5 @@ client = SearchTicker()
 # search_tickers_output_csv(acoes_brasileiras)
 
 #evemplo de consulta de 1 ticker
-#ticker = client.get_info_ticker("BBDC4")
-#print(ticker)
+ticker = client.get_info_ticker("VALE3")
+search_ticker_output_csv(ticker)
--- src/pesquisa_acoes/utils/escreve_csv.py
+++ src/pesquisa_acoes/utils/escreve_csv.py
@@ -11,7 +11,7 @@ def search_ticker_output_csv(dados_dicionario):
         return logger.info("insira o código da ação")
     dados = dados_dicionario.keys()
     try:
-        with open("assests/tickers.csv", "a", encoding="utf8", newline="") as f:
+        with open("assests/tickers.csv", "a", encoding="utf8") as f:
             writer = csv.DictWriter(f, fieldnames=dados)
             writer.writerow(dados_dicionario)
     except Exception as e:
